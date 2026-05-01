f15eac9201603cc144f9df0876614db4bc14fd4f
vishal57-2003 <vishalgiri572003@gmail.com>
2026-05-01 16:48:38 +0530

    Fix MongoDB connection with proper options and timeout settings

--- server.js
+++ server.js
@@ -24,7 +24,12 @@ app.use(cors({
 app.use(express.json());
 
 // Connect to MongoDB
-mongoose.connect(process.env.MONGODB_URI)
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverSelectionTimeoutMS: 5000,
+  socketTimeoutMS: 45000,
+})
   .then(() => console.log('Connected to MongoDB'))
   .catch(err => console.error('MongoDB connection error:', err));
 
